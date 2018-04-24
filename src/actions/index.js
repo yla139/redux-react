e0e3f169504b92c1b68f410c6912c1319c6c69ba
yla139 <yla139.lin@gmail.com>
2018-04-23 21:14:51 -0700

    active book

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
 // selection action
 export function selectBook(book) {
-    console.log('A book has been selected:', book.title);
+    // selectBook is an action creator, returns an action
+    // an object with a type property
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
 }
